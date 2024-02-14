14d19372b91fc0435d95bfe6bf3f27f5d44f1363
Maksim <raymax.spb@gmail.com>
2024-02-14 12:14:53 +0300

    Домашнне задание 3

--- src/Main.java
+++ src/Main.java
@@ -19,34 +19,41 @@ public class Main {
         short f1 = -159;
         short g1 = 27897;
         byte i1 = 67;
-        int lP = 23;
-        int aS = 27;
-        int eA = 30;
+        short lP = 23;
+        short aS = 27;
+        short eA = 30;
         int result1 = 480 / (lP + aS + eA);
         System.out.println("На каждого ученика расчитано " + "480/(lP+aS+eA) " + result1 + " листов бумаги");
-        int bottle = 16;
-        int time = 2;
-        int t1 = 20;
+        byte bottle = 16;
+        byte time = 2;
+        byte t1 = 20;
         int result2 = (t1 / time) * bottle;
         System.out.println(" За " + t1 + " машина произвела " + " (t1/time)*bottle " + result2 + " штук бутылок");
-        int t2 = 24;
-        int t3 = 60;
-        int t5 = 1;
+        byte t2 = 24;
+        byte t3 = 60;
+        byte t5 = 1;
         int result3 = ((t2 * t3) / time) * bottle;
         System.out.println(" За " + t5 + " машина произвела " + " ((t2 * t3) / time) * bottle " + result3 + " штук бутылок");
-        int t6 = 3;
+        byte t6 = 3;
         int result4 = t6 * result3;
         System.out.println(" За " + t6 + " машина произвела " + " t6 * result3 " + result4 + " штук бутылок");
-        int t7 = 31;
+        byte t7 = 31;
         int result5 = t7 * result3;
         System.out.println(" За " + t7 + " машина произвела " + " t7 * result3 " + result5 + " штук бутылок");
-        int totalBottle = 120;
-        int whiteBottle = 2;
-        int brownBottle = 4;
+        byte totalBottle = 120;
+        byte whiteBottle = 2;
+        byte brownBottle = 4;
         int result6 = totalBottle/(whiteBottle+brownBottle);
         int result7 = result6 * whiteBottle;
         int result8 = result6 * brownBottle;
         System.out.println(" В школе, где " + "totalBottle/(whiteBottle+brownBottle) " + result6 + " классов, нужно " + " result6 * whiteBottle " + result7 + " банок белой краски и " + " result6 * brownBottle " + result8 + " банок коричневой краски.");
+        byte banana = 80;
+        byte milk = 105;
+        byte iceCream = 100;
+        byte egg = 70;
+        int result9 = (banana*5)+(milk*2)+(iceCream*2)+(egg*4);
+        System.out.println(" (banana*5)+(milk*2)+(iceCream*2)+(egg*4) "+ result9);
+
 
 
 
