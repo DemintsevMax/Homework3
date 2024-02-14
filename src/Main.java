2aafb6c1b408642debafd04e5324b1c25b01ce91
Maksim <raymax.spb@gmail.com>
2024-02-14 11:09:21 +0300

    Домашнне задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         byte a = 1;
-        System.out.println("Значение переменной с типом byte "+" ровно "+ a);
+        System.out.println("Значение переменной с типом byte " + " ровно " + a);
         short b = 12342;
-        System.out.println("Значение переменной с типом short "+" ровно "+ b);
+        System.out.println("Значение переменной с типом short " + " ровно " + b);
         int c = 413434;
-        System.out.println("Значение переменной с типом int "+" ровно "+ c);
+        System.out.println("Значение переменной с типом int " + " ровно " + c);
         long d = 56434353;
-        System.out.println("Значение переменной с типом long "+" ровно "+ d);
+        System.out.println("Значение переменной с типом long " + " ровно " + d);
         float e = 1.5454344f;
-        System.out.println("Значение переменной с типом float "+" ровно "+ e);
+        System.out.println("Значение переменной с типом float " + " ровно " + e);
         double f = 2.545434489964169;
-        System.out.println("Значение переменной с типом double "+" ровно "+ f);
+        System.out.println("Значение переменной с типом double " + " ровно " + f);
         float a1 = 27.12f;
         float b1 = 987678965549f;
         float c1 = 2.786f;
@@ -22,7 +22,17 @@ public class Main {
         int lP = 23;
         int aS = 27;
         int eA = 30;
-        int result1 = 480/(lP+aS+eA);
-        System.out.println("На каждого ученика расчитано " + "480%(lP+aS+eA) " + result1 + " листов бумаги");
+        int result1 = 480 / (lP + aS + eA);
+        System.out.println("На каждого ученика расчитано " + "480/(lP+aS+eA) " + result1 + " листов бумаги");
+        int bottle = 16;
+        int time = 2;
+        int t1 = 20;
+        int result2 = (t1 / time) * bottle;
+        System.out.println(" За " + t1 + " машина произвела " + " (t1/time)*bottle " + result2 + " штук бутылок");
+        int t2 = 24;
+        int t3 = 60;
+        int t5 = 1;
+        int result3 = ((t2 * t3) / time) * bottle;
+        System.out.println(" За " + t5 + " машина произвела " + " ((t2 * t3) / time) * bottle " + result3 + " штук бутылок");
     }
 }
