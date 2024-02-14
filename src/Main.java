b9eddb26f4c634fe648d8fb6651fe6f33570d962
Maksim <raymax.spb@gmail.com>
2024-02-14 08:37:22 +0300

    1 Задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a = 1;
+        System.out.println("Значение переменной с типом byte "+" ровно "+ a);
+        short b = 12342;
+        System.out.println("Значение переменной с типом short "+" ровно "+ b);
+        int c = 413434;
+        System.out.println("Значение переменной с типом int "+" ровно "+ c);
+        long d = 56434353;
+        System.out.println("Значение переменной с типом long "+" ровно "+ d);
+        float e = 1.5454344f;
+        System.out.println("Значение переменной с типом float "+" ровно "+ e);
+        double f = 2.545434489964169;
+        System.out.println("Значение переменной с типом double "+" ровно "+ f);
+
     }
 }
